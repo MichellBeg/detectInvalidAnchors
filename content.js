59b80fe50d47e649104faec79c2d6aab43f39f56
rsc-miyoshi <tomoya-miyoshi@rscsoft.co.jp>
2023-07-20 11:56:38 +0900

    コンソール出力の内容を修正

--- content.js
+++ content.js
@@ -1,21 +1,21 @@
 window.onload = function () {
-    console.log('Running detectInvalidAnchors()');
-
     // Get all <a> elements in the current web page
     var links = document.getElementsByTagName('a');
 
     // Iterate over all <a> elements
+    console.log('Found <a> without href or with empty href');
+    console.log('========== START ==========');
+
     for (var i = 0; i < links.length; i++) {
         var link = links[i];
 
         // If href is not set or is empty
         if (!link.hasAttribute('href') || link.getAttribute('href').trim() === '') {
-            console.log('Found <a> without href or with empty href: ', link);
+            console.log(link);
 
             // Addclass that highlight content to the <a> element
             link.classList.add('invalid-href');
         }
     }
-
-    console.log('detectInvalidAnchors() finished');
+    console.log('=========== END ===========');
 };
